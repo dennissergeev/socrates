e8772aa2b5856c6ccc1fdf05aa7e068cdaf9a0a2
Denis Sergeev <dennis.sergeev@gmail.com>
2026-07-22 16:10:18 +0100

    Finish the highres SW makefile

--- examples/uranus/calc_pathlengths.py
+++ examples/uranus/calc_pathlengths.py
@@ -9,13 +9,12 @@ Global Average H2-He Path: 3.3274e+05 kg^2/m^5
 Table of absorbers with computed annual global mmr values and total column paths
 
 Gas    Mean MMR     Total column path (kg/m2)
+
+CH4    1.8531e-01   1.0446e+05
 H2     7.4042e-01   4.1737e+05
 He     2.5947e-01   1.4626e+05
-CH4    1.8531e-01   1.0446e+05
-H2O    9.2701e-02   5.2255e+04
-C2H6   2.3059e-09   1.2998e-03
 C2H2   1.1778e-09   6.6390e-04
-CO     1.2139e-09   6.8428e-04
+C2H6   2.3059e-09   1.2998e-03
 """
 
 import pandas as pd
@@ -59,18 +58,11 @@ print(f"Global Average H2-He Path: {total_h2he:.4e} kg^2/m^5")
 
 # Calculate pathlengths for normal absorbers (kg/m2) and their mean MMR
 gases = [
+    "CH4",
     "H2",
     "He",
-    "CH4",
-    "H2O",
-    "NH3",
-    "H2S",
-    "C2H6",
     "C2H2",
-    "C2H4",
-    "CO",
-    "CO2",
-    "PH3",
+    "C2H6",
 ]
 
 print(
